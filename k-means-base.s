0860731c14681a81bef3a822f11857ebb85ea131
Miguel Raposo <migpovrap@hotmail.com>
2024-05-22 15:22:30 +0100

    função de numeros random a funcionar de forma correta

--- k-means-base.s
+++ k-means-base.s
@@ -57,7 +57,7 @@ k:           .word 1
 
 #Dados usados no algoritmo LCG para gerar numeros pseudo aleatorios
 seed:       .word 12345        # Valor da seed
-a:          .word 1664525      # Numero alatorio para multiplicar
+a:          .word 1664525      # Numero aleatorio para multiplicar
 c:          .word 1013904223   # Incremento
 m:          .word 32           # Valor maximo gerar entre 0 e 32
 
@@ -346,6 +346,7 @@ nearestCluster:
     add a0 s2 zero
     jr ra
 
+
 ### randomgencord
 #Gera um par de coordenadas aleatórias
 # Argumentos: nenhum
@@ -354,32 +355,56 @@ nearestCluster:
 # s1: y
 
 randomgencord:
-    lw t0 seed
-    lw t1 a
-    lw t2 c
-    lw t3 m
 
-    mul t5 t0 t1
-    add t5 t5 t2
-    rem t0 t5 t3
+    li a7 30 #System Call usada para carregar o tempo em (ms) os primeiros 32bits no reg a0 e os segundos no reg a1 
+    ecall
+    
+    
+    lw t0, seed                # Carrega a seed inicial
+    add t0, t0, a0             # Adiciona os primeiros 32Bits a seed
+    
+    slli t1, a1, 16            # Executa um ssli dos 16 bits nos segundos 32bits 
+    add t0, t0, t1             # Adiciona o valor obtido pela operacao acima a seed
 
-    add s0 t0 zero
+    # Carrega constantes necessárias para o LCG
+    lw t1, a
+    lw t2, c
+    lw t3, m
 
-    lw t0 seed
-    lw t1 a
-    lw t2 c
-    lw t3 m
+    # Executa o LCG uma vez (Cordenada X)
+    mul t4, t0, t1             # t4 = (seed + tempo) * a
+    add t4, t4, t2             # t4 = t4 + c
+    rem t0, t4, t3             # t0 = t4 % m
+    la t5 seed
+    sw t0  0(t5)                # Guarda a nova seed
+    bgez t0 positive1           #Usado para verificar se o numero gerado nao e negativo
+    neg t0 t0                   #Se este for passa a positivo
+    positive1:
+    add s0, t0, zero           # Guarda a primeiro cordenada X
 
-    mul t5 t0 t1
-    add t5 t5 t2
-    rem t0 t5 t3
+    # Executa o LCG segunda vez (Cordenada Y)
+    lw t0, seed                # Carrega a nova seed
+    lw t1, a
+    lw t2, c
+    lw t3, m
 
-    add s1 t0 zero
+    mul t4, t0, t1             # t4 = seed * a
+    add t4, t4, t2             # t4 = t4 + c
+    rem t0, t4, t3             # t0 = t4 % m
+    la t5 seed
+    sw t0 0(t5)                # Guarda a nova seed
+    bgez t0 positive2
+    neg t0 t0
+    positive2:
+    add s1, t0, zero           # Guarda a segunda cordenada Y
+
     jr ra
-
+    
+    
 initializeCentroids:
     la t0 centroids
     lw t1 k
+    addi t1 t1 -1
     loopgencord:
         addi sp sp -12
         sw t0 0(sp)
@@ -399,6 +424,7 @@ initializeCentroids:
         addi t1 t1 -1
         bgez t1 loopgencord
     jr ra
+
 
 ### mainKMeans
 # Executa o algoritmo *k-means*.
