c71ae6c209350a192e19baa31fce1ef438deec66
Miguel Raposo <migpovrap@hotmail.com>
2024-05-29 14:38:40 +0100

    small bug correted and removed debug calls

--- k-means-base.s
+++ k-means-base.s
@@ -308,7 +308,7 @@ calculateCentroids:
             addi s1 s1 -1
             bltz s1 fimloop
                 sumcords:
-                    blez t4 loopkcluster
+                    bltz t4 loopkcluster
                     la t0 clusters
                     slli t1 t4 4
                     add t0 t0 t1
@@ -319,27 +319,6 @@ calculateCentroids:
                     bne s1 t1 sumcords
                     add s2 s2 t2        #Acumulador Cord X
                     add s3 s3 t3        #Acumulador Cord Y
-
-                    la a0, space
-                    li a7 4
-                    ecall
-
-                    add a0 t2 zero
-                    li a7 1
-                    ecall
-
-                    la a0, virg
-                    li a7 4
-                    ecall
-
-                    add a0 t3 zero
-                    li a7 1
-                    ecall
-
-                    la a0, space
-                    li a7 4
-                    ecall
-
                     addi s4 s4 1        
                     bgez t4 sumcords
 
@@ -352,38 +331,6 @@ calculateCentroids:
             add t0 t0 t1
             sw s2 0(t0)
             sw s3 4(t0)
-
-                    la a0, space
-                    li a7 4
-                    ecall
-
-                    add a0 s2 zero
-                    li a7 1
-                    ecall
-
-                    la a0, space
-                    li a7 4
-                    ecall
-
-                    add a0 s3 zero
-                    li a7 1
-                    ecall
-
-                    la a0, space
-                    li a7 4
-                    ecall
-
-                    la a0, virg
-                    li a7 4
-                    ecall
-
-                    add a0 s4 zero
-                    li a7 1
-                    ecall
-
-                    la a0, newline
-                    li a7 4
-                    ecall
 
             li s2 0                    #Acumulador Cord X
             li s3 0                    #Acumulador Cord Y
